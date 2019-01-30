b8ab7cfac79d77c4d9410ebd37dbc7eb45735053
Sergey Kosik <sergey.kosik@ocrex.com>
2019-01-30 10:45:42 +0000

    Corrected sending notification on all promises complete.

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ const diffUrl = config.bbRepoUrl + 'diff/';
 
 let changedCommits = [];
 
-var authObj = { 
-  'user': config.bbUser,
-  'pass': config.bbPass,
-  'sendImmediately': true
+var authObj = {
+  user: config.bbUser,
+  pass: config.bbPass,
+  sendImmediately: true
 };
 
 // console.log('config.watchList', config.watchList);
 
 // '3671a93f7af9d1f51aecc06933da25a1899aee47'  -- one file
-// sergey: 'e224e15', '90da35e'; 
+// sergey: 'e224e15', '90da35e';
 // let commitHash = 'e224e15'; //'8f349818c99d1968f3e9ae690089e99b8a6f42f9';
 
 listCommits();
@@ -34,97 +34,100 @@ listCommits();
 
 function listCommits() {
   // Make 4 paged requests which would return 4*30 commits, which should cover about 2 days
-  var request1 = rp({ url:commitsUrl+'1', method: 'GET', auth: authObj, json: true });
-  var request2 = rp({ url:commitsUrl+'2', method: 'GET', auth: authObj, json: true });
-  var request3 = rp({ url:commitsUrl+'3', method: 'GET', auth: authObj, json: true });
-  var request4 = rp({ url:commitsUrl+'4', method: 'GET', auth: authObj, json: true });
+  var request1 = rp({ url: commitsUrl + '1', method: 'GET', auth: authObj, json: true });
+  var request2 = rp({ url: commitsUrl + '2', method: 'GET', auth: authObj, json: true });
+  var request3 = rp({ url: commitsUrl + '3', method: 'GET', auth: authObj, json: true });
+  var request4 = rp({ url: commitsUrl + '4', method: 'GET', auth: authObj, json: true });
 
   Bluebird.all([request1, request2, request3, request4])
-      .spread(function (res1, res2, res3, res4) {
-          var commits = _.concat(res1.values, res2.values, res3.values, res4.values);
+    .spread(function(res1, res2, res3, res4) {
+      var commits = _.concat(res1.values, res2.values, res3.values, res4.values);
 
-          const mapped = mapCommits(commits);
-          checkCommits(mapped);
-
-          // TODO: sendNotification should be called only after all getDiffStat completed.
-          sendNotification();
-      })
-      .catch(function (err) {
-        console.log('Get commits failed', err);
-      });
+      const mapped = mapCommits(commits);
+      console.log('commits num: ', mapped.length);
+      checkCommits(mapped);
+    })
+    .catch(function(err) {
+      console.log('Get commits failed', err);
+    });
 }
 
 function checkCommits(commits) {
+  var promises = [];
   for (let i = 0; i < commits.length; i++) {
-    _.delay(getDiffStat, 500, commits[i]);
+    promises.push(getDiffStat(commits[i]));
   }
+
+  Bluebird.all(promises).then(function(res) {
+    console.log('all completed', changedCommits);
+    sendNotification();
+  });
 }
-  
 
 function getDiffStat(commit) {
-  var difReq = rp({ url:diffStatUrl + commit.hash, method: 'GET', auth: authObj, json: true });
-  
-  difReq.then(function (res) {
-    // console.log('Dif', res);
+  return rp({ url: diffStatUrl + commit.hash, method: 'GET', auth: authObj, json: true })
+    .then(function(res) {
+      // console.log('Dif', res);
 
-    const paths = [];
-    const diffs = res.values || [];
+      const paths = [];
+      const diffs = res.values || [];
 
-    _.each(diffs, diff => {
+      _.each(diffs, diff => {
+        // get path object which could be
+        // either old (modified, deleted);
+        // or new (added)
+        var changed = diff.old || diff.new;
+        paths.push(changed.path);
+      });
 
-      // get path object which could be
-      // either old (modified, deleted);
-      // or new (added)
-      var changed = diff.old || diff.new;
-      paths.push(changed.path);
-      // console.log('paths', paths);
+      const isChanged = isAnyWachedChanged(paths);
+
+      if (isChanged) {
+        commit.paths = paths;
+        changedCommits.push(commit);
+      }
+
+      return this;
+    })
+    .catch(function(err) {
+      console.log('getDiffStat error', err);
+      return this;
     });
-
-    const isChanged = isAnyWachedChanged(paths);
-
-    if(isChanged) {
-      commit.paths = paths;
-      changedCommits.push(commit);
-    }
-
-  })
-  .catch(function (err) {
-    console.log('getDiffStat error', err);
-  });
 }
 
 function showDiff(hash) {
-  var difReq = rp({ url:diffUrl+hash, method: 'GET', auth: authObj, json: true });
-  
-  difReq.then(function (res) {
-    console.log('Dif', res);
-  })
-  .catch(function (err) {
-    console.log('showDiff error', err);
-  });
+  var difReq = rp({ url: diffUrl + hash, method: 'GET', auth: authObj, json: true });
+
+  difReq
+    .then(function(res) {
+      console.log('Dif', res);
+    })
+    .catch(function(err) {
+      console.log('showDiff error', err);
+    });
 }
 
 function mapCommits(data) {
-   
   let comparedDate = moment();
 
-   var filtered = _.filter(data, commit => {
+  var filtered = _.filter(data, commit => {
     // return moment(commit.date).isAfter('2019-01-24', 'day');
     return moment(commit.date).isSame(comparedDate, 'date');
-   });
-   
-   var arr = _.map(filtered, commit => {
-     return { 
-       hash: commit.hash, 
-       message: commit.message,
-       author: commit.author.raw, 
-       date: moment(commit.date).format('LLL') };
-   });
+  });
 
-   // console.log('commits', arr);
-   // console.log('filtered count', arr.length);
+  var arr = _.map(filtered, commit => {
+    return {
+      hash: commit.hash,
+      message: commit.message,
+      author: commit.author.raw,
+      date: moment(commit.date).format('LLL')
+    };
+  });
 
-   return arr;
+  // console.log('commits', arr);
+  // console.log('filtered count', arr.length);
+
+  return arr;
 }
 
 function isAnyWachedChanged(paths) {
@@ -132,11 +135,15 @@ function isAnyWachedChanged(paths) {
 
   _.each(paths, path => {
     found = config.watchList.some(interested => path.indexOf(interested) > -1);
-    if(found) {
+    if (found) {
       return;
     }
-  }); ;
-  console.log('isAnyWatchedChanged', found);
+  });
+
+  if(found) {
+    console.log('isAnyWatchedChanged', found);
+  }
+  
   return found;
 }
 
@@ -144,7 +151,7 @@ function sendNotification() {
   if (changedCommits.length === 0) {
     return;
   }
-  
+
   let content = '';
   _.each(changedCommits, changed => {
     content += buildCommitContent(changed);
@@ -152,18 +159,22 @@ function sendNotification() {
   });
 
   console.log('email content', content);
-  
+
   sendEmail(content).catch(console.error);
 }
-  
+
 function buildCommitContent(commit) {
   let content = `
-  <h4>Commit: ${commit.hash}</h4>
-  <h4>Author: ${commit.author}</h4>
-  <h4>Date: ${commit.date}</h4>
-  <h4>Message:</h4>
-  <p>${commit.message}</p>
+  <b>Commit:</b> ${commit.hash}<br/>
+  <b>Author:</b> ${commit.author}<br/>
+  <b>Date:</b> ${commit.date}<br/>
+  <b>Message:</b><br/>
+  ${commit.message} <br/><br/>
+  <b>Changes:</b><br/>
   ${buildPathsContent(commit.paths)}
+  <br/>
+  <hr>
+  <h5>Sent by Bitbucket-Repo-Notifier</h5>
   `;
 
   return content;
@@ -178,8 +189,8 @@ function buildPathsContent(paths) {
   return `<table>${rows}</table>`;
 }
 
-async function sendEmail(paths) {
-// Generate test SMTP service account from ethereal.email
+async function sendEmail(body) {
+  // Generate test SMTP service account from ethereal.email
   // Only needed if you don't have a real mail account for testing
   // let account = await nodemailer.createTestAccount();
 
@@ -196,16 +207,13 @@ async function sendEmail(paths) {
   let mailOptions = {
     from: `"Bitbucket Notifier" <${config.emailFrom}>`, // sender address
     to: config.emailTo, // list of receivers
-    subject: "Docurec repo changes", // Subject line
-    text: "", // plain text body
-    html: "<b>List of modified files:</b><br />" + buildEmailContent(paths) // html body
+    subject: 'Docurec repo changes', // Subject line
+    text: '', // plain text body
+    html: body // html body
   };
 
   // send mail with defined transport object
-  let info = await transporter.sendMail(mailOptions)
+  let info = await transporter.sendMail(mailOptions);
 
-  console.log("Message sent: %s", info.messageId);
+  console.log('Message sent: %s', info.messageId);
 }
-
-
-
